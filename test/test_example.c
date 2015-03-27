2803a13f840a2801c28351c312d27a5f4e4363e2
Miguel Marco <mmarco@unizar.es>
2015-03-28 00:47:59 +0100

    Added test

---
+++ test/test_example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "homfly.h"
+#include <gc.h>
+
+int main()
+{
+    char *out;
+    char input[] = " 1 6 0 1 1 -1  2 1  0 -1  1 1  2 -1 0 1 1 1 2 1 ";
+    out = homfly(input);
+    printf("%s", out);
+    if (out == " - L^-4 - 2L^-2 + M^2L^-2")
+    {
+        printf("error in computation");
+        return 1;
+    };
+    printf("correct result");
+    return 0;
+}
